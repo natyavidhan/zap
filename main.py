4f2f1232ff4b415119fec7288cb43296fd3cd2fb
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-01 12:51:05 +0530

    signin-with-google

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config.from_mapping(dict(os.environ))
+
+oauth = OAuth(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'user'not in session:
+        return render_template("index.html")
+
+
+@app.route('/google/')
+def google():
+    GOOGLE_CLIENT_ID = os.environ.get('G_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('G_CLIENT_SECRET')
+    
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+    redirect_uri = url_for('google_auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@app.route('/google/auth')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token)
+    print(" Google User ", user)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run("localhost", debug=True)
