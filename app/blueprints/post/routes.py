1e9ba17d9e0566ca827287addf0cb5617f3d382c
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-08-06 03:10:30 +0530

    post routes new auth implemented

--- app/blueprints/post/routes.py
+++ app/blueprints/post/routes.py
@@ -2,6 +2,7 @@ from flask import session, request, redirect, render_template, url_for, jsonify
 
 from app import db
 from app.blueprints.post import bp
+from app.blueprints.auth import utils
 
 @bp.route("/new", methods=["GET", "POST"])
 def new():
@@ -12,17 +13,19 @@ def new():
 
     if request.method == "GET":
         return render_template("new.html", error=error)
+
+    session_obj = utils.get_current_user()
+    if session_obj is False:
+        return "False"
     
     image   = request.files.get("image")
     caption = request.form.get("caption")
     tags    = [i.strip() for i in request.form.get("tags").split(",") if i.strip() != ""]
 
-    print(dict(request.form))
-
     if image is None or caption.strip() == "":
         return redirect("/new?error=No image/caption provided")
     
-    post = db.create_post(session['user']['_id'], caption, image, tags)
+    post = db.create_post(session_obj['_id'], caption, image, tags)
 
     return redirect(f"/post/{post['_id']}")
 
@@ -35,7 +38,11 @@ def post(post_id):
     is_json = request.args.get("json")
     user = db.fetch_user("_id", post['user'])
     if 'user' in session:
-        liked = session['user']['_id'] in post['likes']
+        session_obj = utils.get_current_user()
+        if session_obj is False:
+            liked = False
+        else:
+            liked = session_obj['_id'] in post['likes']
     else:
         liked = False
     if is_json:
@@ -46,9 +53,14 @@ def post(post_id):
 def comment():
     if 'user' not in session:
         return "False"
+    
+    session_obj = utils.get_current_user()
+    if session_obj is False:
+        return "False"
+
     post = request.args.get("post")
     comment = request.args.get("comment")
-    res = db.add_comment(session['user']['_id'], post, comment)
+    res = db.add_comment(session_obj['_id'], post, comment)
     if res:
         return "True"
     return "False"
@@ -57,8 +69,13 @@ def comment():
 def like():
     if 'user' not in session:
         return "False"
+    
+    session_obj = utils.get_current_user()
+    if session_obj is False:
+        return "False"
+
     post = request.args.get("post")
-    res = db.toggle_like(session['user']['_id'], post)
+    res = db.toggle_like(session_obj['_id'], post)
     if res:
         return "True"
     return "False"
